86933aa5524b9691f246f6fc747b42673371721c
lekshmisree@gmail.com <lekshmisree89@gmail.com>
2024-10-26 13:28:42 -0400

    modiified index.ts in api routes

--- src/routes/api/appRoutes.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-const router = Router();
-
-import userRoutes from './userRoutes.js';
-import thoughtRoutes from './thoughtRoutes.js';
-
-router.use(userRoutes);
-router.use(thoughtRoutes);
-
